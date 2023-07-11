64fa6fb85b797a34032ed06cd1c169680e97dd44
iSplasher <isplasher@gmail.com>
2023-07-11 15:08:53 +0200

    feat: added cache settings;

--- src/components/settings.ts
+++ src/components/settings.ts
@@ -1,10 +1,13 @@
 import { App, PluginSettingTab, Setting } from "obsidian";
 
-import type MarkPlacePlugin from "../main";
+import { CLASSES } from "../constants";
 
+import type MarkPlacePlugin from "../main";
 export const DEFAULT_SETTINGS = {
 	showError: "modal" as "modal" | "notice" | "none",
 	showNotice: {},
+	cache: true,
+	cachePath: "",
 	liveRendering: false,
 };
 
@@ -23,15 +26,21 @@ export default class MarkPlaceSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl("h2", { text: "MarkPlace" });
+		containerEl.addClass(CLASSES.settings);
+
+		containerEl.createEl("h1", { text: "MarkPlace" });
+
+		const createHeader = (text: string) =>
+			containerEl.createEl("h2", { text });
 
 		const descEl = containerEl.createDiv();
-		descEl.append(
-			"MarkPlace is a templating plugin that allows you to render render text directly inside your markdown.",
-			descEl.createEl("br"),
-			"Syntax: ",
+
+		const usageEl = containerEl.createEl("blockquote");
+
+		usageEl.append(
+			"Tag syntax: ",
 			descEl.createEl("code", {
-				text: "%{modifier?} your block name % content % end %",
+				text: "%%{{modifier?} your block name }%% content %%{ end }%%",
 			}),
 			descEl.createEl("br"),
 			"Modifiers decide when to render, choices are ",
@@ -42,8 +51,72 @@ export default class MarkPlaceSettingTab extends PluginSettingTab {
 			descEl.createEl("code", {
 				text: "'*'",
 			}),
-			" (for delayed) or nothing (default)."
+			" (for delayed) or nothing (default).",
+			descEl.createEl("br"),
+			"To escape the tag syntax, use ",
+			descEl.createEl("code", {
+				text: "'\\'",
+			}),
+			" like this: ",
+			descEl.createEl("code", {
+				text: "%%{ your block \\}%% name }%% some-\\%%{-conent %%{ end }%%",
+			})
 		);
+
+		descEl.append(
+			"MarkPlace is a templating plugin that allows you to render text directly within your markdown files.",
+			descEl.createEl("br"),
+			descEl.createEl("br"),
+			descEl.createEl("b", { text: "Usage:" }),
+			descEl.createEl("br"),
+			usageEl,
+			descEl.createEl("br")
+		);
+
+		createHeader("Cache");
+
+		const cacheDesc = document.createDocumentFragment();
+		cacheDesc.append(
+			"MarkPlace is able to cache the original content of blocks.",
+			document.createElement("br"),
+			"This allows you to still work with the original content after a block has rendered.",
+			document.createElement("br"),
+			"The cache is stored to a file in a JSON format, and is not meant to be edited by hand.",
+			document.createElement("br"),
+			"You can however, delete the file to reset the cache."
+		);
+
+		new Setting(containerEl).setDesc(cacheDesc).setDisabled(true);
+
+		new Setting(containerEl).setName("Enable cache").addToggle((toggle) => {
+			toggle
+				.setValue(this.plugin.settings.cache)
+				.onChange(async (value: MarkPlacePluginSettings["cache"]) => {
+					this.plugin.settings.cache = value;
+					await this.plugin.saveSettings();
+				});
+		});
+
+		const cacheFileDesc = document.createDocumentFragment();
+		cacheFileDesc.append(
+			"The path must point to a location inside your vault. If the file does not exist, it will be created with a .json extension."
+		);
+		new Setting(containerEl)
+			.setName("Cache file location")
+			.setDesc(cacheFileDesc)
+			.setDisabled(true);
+
+		const cacheFileEl = new Setting(containerEl).addText((text) => {
+			text.setPlaceholder("Example: path/to/cache/file.json")
+				.setValue(this.plugin.settings.cachePath)
+				.onChange(async (value) => {
+					this.plugin.settings.cachePath = value;
+					await this.plugin.saveSettings();
+				});
+		});
+		cacheFileEl.controlEl.addClass(CLASSES.settingsWideControl);
+
+		createHeader("Advanced");
 
 		new Setting(containerEl)
 			.setName("Report error")
@@ -68,11 +141,18 @@ export default class MarkPlaceSettingTab extends PluginSettingTab {
 					);
 			});
 
+		const liveRendDesc = document.createDocumentFragment();
+		liveRendDesc.append(
+			"Block rendering will always occur in reading mode, however, you can toggle this on to also render when live editing.",
+			document.createElement("br"),
+			"This may result in a janky editing experience, so enable with caution.",
+			document.createElement("br"),
+			"Note: rendering will never occur in source mode."
+		);
+
 		new Setting(containerEl)
-			.setName("Live rendering")
-			.setDesc(
-				"Rendering will always occur in reading mode, however, you can toggle this to also render when live editing."
-			)
+			.setName("Live Rendering")
+			.setDesc(liveRendDesc)
 			.addToggle((toggle) => {
 				toggle
 					.setValue(this.plugin.settings.liveRendering)
@@ -85,5 +165,30 @@ export default class MarkPlaceSettingTab extends PluginSettingTab {
 						}
 					);
 			});
+
+		createHeader("Statistics");
+		const statsDesc = document.createDocumentFragment();
+
+		const statEl = (name: string, value: string) => {
+			const el = statsDesc.createSpan({ cls: CLASSES.stat });
+			el.dataset.markplaceStatName = name;
+			el.append(
+				el.createEl("b", { text: `${name}: ` }),
+				el.createSpan({ cls: CLASSES.statValue, text: value })
+			);
+
+			return el;
+		};
+
+		statsDesc.append(
+			statEl("Blocks", "0"),
+			document.createElement("br"),
+			statEl("Cached blocks", "0"),
+			document.createElement("br"),
+			statEl("Blocks with non-unique identifiers (in same file)", "0"),
+			document.createElement("br")
+		);
+
+		new Setting(containerEl).setDesc(statsDesc).setDisabled(true);
 	}
 }
--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,16 @@ export const CLASSES = {
 	modal: "markplace-modal",
 	error: "markplace-error",
 	errorDetails: "markplace-error-details",
+
+	// settings classes
+	settings: "markplace-settings",
+	settingsWideControl: "markplace-wide-control",
+	settingsExpandedControl: "markplace-expanded-control",
+	stat: "markplace-stat",
+	statValue: "markplace-stat-value",
+
+	// misc classes
+	indent: "markplace-indent",
 };
 
 export class constant {
